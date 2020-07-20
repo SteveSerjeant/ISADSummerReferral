18d87293d95e1b5f8683a500b21bee6419bb0290
Steve Serjeant <steven.serjeant@students.plymouth.ac.uk>
2020-07-20 21:31:45 +0100

    Create folders for file structure

---
+++ public/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html lang = "en">
+<head>
+<meta charset="UTF-8">
+<title>Getting Started in PHP</title>
+</head>
+<body>
+
+</body>
+</html>
+
+
+<?php
+#Write the traditional greeting
+echo '<h1>Hello World!</h1>';
+
+?>
+
+<p>Ignored by PHP and displayed by the browser</p>
+
+<?php echo 'Script code that will be parsed';?>
+
+<p>Also ignored by PHP and displayed by the browser</p>
+
+<?= 'Hello!';?> is equivalent to <?php echo 'Hello!'; ?>
+
+<?php echo 'First statement '; // A single line comment
+
+/* This is a multi line comment
+containing two lines of comments. */
+
+echo ' Second statement';?>
+
+<?php echo 'First statement '; 
+
+echo 'Second statement ' #Another single line comment.
+?> 
