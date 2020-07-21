39f4e89dc4f32938c3e9cb957d96da752eff6ba2
Steve Serjeant <steven.serjeant@students.plymouth.ac.uk>
2020-07-21 11:24:18 +0100

    inclusion of 3 responsive columns to page

--- public/index.php
+++ public/index.php
@@ -1,15 +1,32 @@
 <!DOCTYPE HTML>
 <html lang = "en">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head>
-<meta charset="UTF-8">
 
-<title>ISAD25 Referral</title>
+<head>
+    <title>ISAD25 Referral</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../assets/css/stylesheet.css" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 
-<h1>ISAD251 Summer Referral</h1>
+<div id="header">?????? Family Appointments and Deadlines Tracker</div>
+
+<div class="container-fluid">
+    <h1>Responsive Columns</h1>
+    <p>Resize the browser window to see the effect.</p>
+    <p>The columns will automatically stack on top of each other when the screen is less than 576px wide.</p>
+    <div class="row">
+        <div class="col-sm-4" id="column" style="background-color:lavender;">The Current Month</div>
+        <div class="col-sm-4" id="column" style="background-color:lavenderblush;">This column will show the following month</div>
+        <div class="col-sm-4" id="column" style="background-color:lavender;">And this will show the next month, 3 months total</div>
+
+    </div>
+</div>
 
 
 <footer>
@@ -22,8 +39,3 @@
 </html>
 
 
-<?php
-#Write the traditional greeting
-echo '<h1>Hello World!</h1>';
-
-?>
